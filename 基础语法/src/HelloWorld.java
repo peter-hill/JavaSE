9d0a62e5490207385cf0399e0141c016d847746c
chenhao <hao.chen14@hand-china.com>
2020-05-12 19:24:38 +0800

    first commit

---
+++ 基础语法/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(Integer.toBinaryString(109));
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
