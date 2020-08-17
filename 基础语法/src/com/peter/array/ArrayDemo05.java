80cf085e53689b53edf6af5b3f15dccf533631f1
hao chen <peterchen2132@outlook.com>
2020-08-17 10:35:54 +0800

    version 0.6

--- 基础语法/src/com/peter/array/ArrayDemo05.java
+++ 基础语法/src/com/peter/array/ArrayDemo05.java
@@ -1,8 +1,21 @@
 package com.peter.array;
 
+import java.util.Arrays;
+
+import static com.peter.array.ArrayDemo03.printArray;
+
 /**
  * @Author ChenHao
  * @Date 2020/7/29
  */
 public class ArrayDemo05 {
+    public static void main(String[] args) {
+        int[][] array = {{1,2},{2,3},{4,5}};
+        printArray(array[0]);
+        for (int[] ints : array) {
+//            printArray(ints);
+        }
+        Arrays()
+    }
+
 }
